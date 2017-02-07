eee534e34e550510dfe5256577e619b0877a26e9
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-02-07 15:50:47 -0800

    getting closer to norm....

--- get_specifier.c
+++ get_specifier.c
@@ -26,18 +26,28 @@ static int		get_prec(char *s, va_list ap)
 	int	prec;
 
 	if (*s == '*')
-	{
 		prec = va_arg(ap, int);
-	}
 	else
-	{
 		prec = ft_atoi_base(s, 10);
-	}
 	if (prec < 0)
-	{
 		prec = 0;
-	}
 	return (prec);
+}
+
+static t_spec	get_width(char *s, t_spec spec, va_list ap)
+{
+	if (*s == '*')
+	{
+		spec.width = va_arg(ap, int);
+		if (spec.width < 0)
+		{
+			spec.flags |= FLAG_LEFT;
+			spec.width *= -1;
+		}
+	}	
+	else
+		spec.width = ft_atoi_base(s, 10);
+	return (spec);
 }
 
 static int		get_flag(char c)
@@ -57,7 +67,7 @@ static int		get_flag(char c)
 
 t_spec			get_spec(char *s, va_list ap)
 {
-	t_spec			spec;
+	t_spec	spec;
 	
 	spec.flags = 0;
 	spec.width = 0;
@@ -65,43 +75,24 @@ t_spec			get_spec(char *s, va_list ap)
 	spec.prec = -1;
 	while (ft_memchr("-+ #*0123456789.hljz", *s, 20))
 	{
-		if (get_flag(*s))		
-			spec.flags |= get_flag(*s++);
-		else if (*s == '*')
-		{
-			spec.width = va_arg(ap, int);
-			if (spec.width < 0)
-			{
-				spec.flags |= FLAG_LEFT;
-				spec.width *= -1;
-			}
-			s++;
-		}
-		else if (ft_memchr("123456789", *s, 9))
-		{
-			spec.width = ft_atoi_base(s, 10);
-			while (ft_isdigit(*s))
-				s++;
-		}
+		if (get_flag(*s))
+			spec.flags |= get_flag(*s);
+		else if (ft_memchr("*123456789", *s, 10))
+			spec = get_width(s, spec, ap);
 		else if (*s == '.')
-		{
 			spec.prec = get_prec(++s, ap);
-			if (*s == '*')
-				s++;
-			else
-			{
-				while (ft_isdigit(*s))
-					s++;
-			}
-		}
 		else if (ft_memchr("hljz", *s, 4))
 		{
 			if (get_len(s) > spec.len)
-				spec.len = get_len(&*s);
+				spec.len = get_len(s);
 			if (ft_memchr("hl", *s, 2) && *s == *(s + 1))
 				s++;
-			s++;
 		}
+		if (ft_memchr("123456789", *s, 9))
+			while (ft_isdigit(*s))
+				s++;
+		else if (ft_memchr("-+ #*0.hljz", *s, 11))
+			s++;
 	}
 	spec.type = *s;
 	if (ft_memchr("DUOpSC", spec.type, 6))
