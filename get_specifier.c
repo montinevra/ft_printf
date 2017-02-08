55fc8a4dd315d22c077a7a7f6c1d42342e422c9e
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-02-07 16:01:38 -0800

    normed and working

--- get_specifier.c
+++ get_specifier.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_specifier.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvan-erp <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/07 16:00:19 by pvan-erp          #+#    #+#             */
+/*   Updated: 2017/02/07 16:00:21 by pvan-erp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 static t_len	get_len(const char *c)
@@ -44,7 +56,7 @@ static t_spec	get_width(char *s, t_spec spec, va_list ap)
 			spec.flags |= FLAG_LEFT;
 			spec.width *= -1;
 		}
-	}	
+	}
 	else
 		spec.width = ft_atoi_base(s, 10);
 	return (spec);
@@ -53,45 +65,39 @@ static t_spec	get_width(char *s, t_spec spec, va_list ap)
 static int		get_flag(char c)
 {
 	if (c == '-')
-		return(FLAG_LEFT);
+		return (FLAG_LEFT);
 	else if (c == '+')
-		return(FLAG_SIGN);
+		return (FLAG_SIGN);
 	else if (c == ' ')
-		return(FLAG_NOSIGN);
+		return (FLAG_NOSIGN);
 	else if (c == '#')
-		return(FLAG_ALT);
+		return (FLAG_ALT);
 	else if (c == '0')
-		return(FLAG_ZERO);
+		return (FLAG_ZERO);
 	return (0);
 }
 
 t_spec			get_spec(char *s, va_list ap)
 {
 	t_spec	spec;
-	
-	spec.flags = 0;
-	spec.width = 0;
-	spec.len = LEN_NONE;
-	spec.prec = -1;
+
+	spec = (t_spec){.flags = 0, .width = 0, .len = LEN_NONE, .prec = -1};
 	while (ft_memchr("-+ #*0123456789.hljz", *s, 20))
 	{
-		if (get_flag(*s))
-			spec.flags |= get_flag(*s);
-		else if (ft_memchr("*123456789", *s, 10))
+		spec.flags |= get_flag(*s);
+		if (ft_memchr("*123456789", *s, 10))
 			spec = get_width(s, spec, ap);
 		else if (*s == '.')
 			spec.prec = get_prec(++s, ap);
 		else if (ft_memchr("hljz", *s, 4))
-		{
 			if (get_len(s) > spec.len)
 				spec.len = get_len(s);
-			if (ft_memchr("hl", *s, 2) && *s == *(s + 1))
-				s++;
-		}
 		if (ft_memchr("123456789", *s, 9))
 			while (ft_isdigit(*s))
 				s++;
 		else if (ft_memchr("-+ #*0.hljz", *s, 11))
+			s++;
+		if (ft_memchr("hl", *s, 2) && *s == *(s - 1))
 			s++;
 	}
 	spec.type = *s;
